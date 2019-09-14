2c34806bc084442c4d311fd04f66d2af7bb6e21c
sajad1993gh <sajad1993gh@gmail.com>
2019-09-14 19:42:41 -0400

    Test Class is being done

--- firstProject/src/TestPainting.java
+++ firstProject/src/TestPainting.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class TestPainting {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hi");
-        System.out.println("Halloo's");
+        System.out.println("TESTING BEGINS:\n");
+
+        Painting painting = new Painting();
+
+        painting.setArtistName("Mark Rothko");
+        painting.setName("No. 6(Violet, Green and Red");
+        painting.setPrice(186000000);
+        painting.setYear(1951);
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.println(painting.getArtistName());
     }
 }
