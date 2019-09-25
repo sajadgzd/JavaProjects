e1d3c9f9081d1170d0d41e8a51ca1f13cf7f96b8
sajad1993gh <sajad1993gh@gmail.com>
2019-09-24 23:36:31 -0400

    added initial file

---
+++ secondProject/src/Main.java
@@ -0,0 +1,12 @@
+// using java.security.SecureRandom
+import java.security.SecureRandom;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%s", "On Your Mark, Get Set, Go");
+
+    }
+}
