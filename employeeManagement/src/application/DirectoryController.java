7404921064f4bcdc5008e9bb1e879f003689286f
sajad1993gh <sajad1993gh@gmail.com>
2019-11-10 21:57:03 -0500

    more initial

---
+++ employeeManagement/src/application/DirectoryController.java
@@ -0,0 +1,65 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+import java.awt.event.ActionEvent;
+
+public class DirectoryController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private Button btnSerialize;
+
+    @FXML
+    private TextField txtFldName;
+
+    @FXML
+    private Label lblCurrRecord;
+
+    @FXML
+    private Button btnLoad;
+
+    @FXML
+    private Button btnNavNext;
+
+    @FXML
+    private Label lblFilename;
+
+    @FXML
+    private Button btnNavDel;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    void buttonClicked(ActionEvent event) {
+
+    }
+
+//    @FXML
+//    void buttonClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void buttonClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void buttonClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void buttonClicked(ActionEvent event) {
+//
+//    }
+
+}
