ea31b5e0b6105180fb811fcf79e1f335c2f963c2
sajad1993gh <sajad1993gh@gmail.com>
2019-11-11 11:49:13 -0500

    building layout

--- employeeManagement/src/application/DirectoryController.java
+++ employeeManagement/src/application/DirectoryController.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,6 +11,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class DirectoryController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private TextField txtFldCompany;
@@ -83,4 +92,21 @@ public class DirectoryController {
 //
 //    }
 
+    @FXML
+    void initialize() {
+        assert txtFldCompany != null : "fx:id=\"txtFldCompany\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert lblCurrRecord != null : "fx:id=\"lblCurrRecord\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnLoad != null : "fx:id=\"btnLoad\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnNavNext != null : "fx:id=\"btnNavNext\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnNavDel != null : "fx:id=\"btnNavDel\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnNavPrev != null : "fx:id=\"btnNavPrev\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert borderPane != null : "fx:id=\"borderPane\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnSerialize != null : "fx:id=\"btnSerialize\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert txtFldName != null : "fx:id=\"txtFldName\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnNavAdd != null : "fx:id=\"btnNavAdd\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert lblFilename != null : "fx:id=\"lblFilename\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert txtFldExtension != null : "fx:id=\"txtFldExtension\" was not injected: check your FXML file 'Directory.fxml'.";
+        assert btnExit != null : "fx:id=\"btnExit\" was not injected: check your FXML file 'Directory.fxml'.";
+
+    }
 }
