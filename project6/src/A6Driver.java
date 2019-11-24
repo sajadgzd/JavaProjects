9f926c0c2cac68c2a6dbda7c33ce8eecb2b5724a
sajad1993gh <sajad1993gh@gmail.com>
2019-11-24 16:36:13 -0500

    cleaned code with first task

--- project6/src/A6Driver.java
+++ project6/src/A6Driver.java
@@ -12,50 +12,37 @@ public class A6Driver {
      * Convert to code using Streams.
      * Do not use for
      */
-    private static StringBuilder sRepeated = new StringBuilder();
-
-//    private static final java.util.stream.Collectors Collectors = ;
 
     static {
-        for (int i = 0; i < 110; i++)
-            separator.append('-');
-//
-//       separators = IntStream.range(0, 110).mapToObj(int -> StringBuffer).collect(Collectors.joining(" "));
-        String s = "-";
-        sRepeated.append(IntStream.range(0, 110).mapToObj(i -> s).collect(Collectors.joining("")));
-//    IntStream.range(0, 110).mapToObj(i -> s).collect(Collectors.joining(""));
+        separator.append(IntStream.range(0, 110).mapToObj(i -> "-").collect(Collectors.joining("")));
     }
 
     public static void main(String[] args) throws FileNotFoundException, ParseException {
 
-        System.out.println("Hey");
-        System.out.println(sRepeated);
-    }
+        NetworkLogManager networkLogManager = new NetworkLogManager();
 
-//        NetworkLogManager networkLogManager = new NetworkLogManager();
-//
-//        System.out.println(networkLogManager);
-//
-//        if (networkLogManager.loadFile("Network.log")) {
-//
-//            System.out.println(separator.toString());
-//
-//            System.out.println(networkLogManager);
-//
-//            System.out.println(separator.toString());
-//
-//            System.out.printf("There are %d records with id 1%n", networkLogManager.searchById("1").size());
-//            System.out.printf("There are %d records with id 9%n", networkLogManager.searchById("9").size());
-//            System.out.printf("There are %d records with protocol TCP %n", networkLogManager.searchByProtocol("TCP").size());
-//            System.out.printf("There are %d records with protocol UDP %n", networkLogManager.searchByProtocol("UDP").size());
-//
-//            System.out.println(separator.toString());
-//
-//            List<LogEntry> list = networkLogManager.searchByRange("Jan 1 2018 00:00:00", "Dec 31 2018 23:59:59");
-//
-//            System.out.printf(String.format("There are %,d entries from 2018", list.size()));
-//        }
-//        else
-//            System.err.println("Failed to load file");
-//    }
+        System.out.println(networkLogManager);
+
+        if (networkLogManager.loadFile("Network.log")) {
+
+            System.out.println(separator.toString());
+
+            System.out.println(networkLogManager);
+
+            System.out.println(separator.toString());
+
+            System.out.printf("There are %d records with id 1%n", networkLogManager.searchById("1").size());
+            System.out.printf("There are %d records with id 9%n", networkLogManager.searchById("9").size());
+            System.out.printf("There are %d records with protocol TCP %n", networkLogManager.searchByProtocol("TCP").size());
+            System.out.printf("There are %d records with protocol UDP %n", networkLogManager.searchByProtocol("UDP").size());
+
+            System.out.println(separator.toString());
+
+            List<LogEntry> list = networkLogManager.searchByRange("Jan 1 2018 00:00:00", "Dec 31 2018 23:59:59");
+
+            System.out.printf(String.format("There are %,d entries from 2018", list.size()));
+        }
+        else
+            System.err.println("Failed to load file");
+    }
 }
